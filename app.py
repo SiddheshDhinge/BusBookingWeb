cf3eb3cb558d8f92019bfeffba05c6aa6262eb7e
Sayali Dhinge <saddy97531@gmail.com>
2022-12-24 16:10:04 +0530

    API files

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ from datetime import timedelta
 connectDB()
 createAllTables()
 # from model.session_manager import getSessionStatus, addActiveSession
+from app import label, label_reason
 from app.controller_owner import ControllerOwner
-from app import controller_customer, label, label_reason
 from app.controller_operator import ControllerOperator
+from app.controller_customer import ControllerCustomer
 
 
 import os
@@ -28,10 +29,6 @@ def index():
 def bookbus():
     return render_template('bookbus.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
 @app.route('/details')
 def details():
     return render_template('details.html')
@@ -46,14 +43,22 @@ def owner():
 
 @app.route('/customer', methods=['POST'])
 def customer():
-    return controller_customer.handleRequest()
+    return ControllerCustomer().handleRequest()
 
 @app.route('/operator', methods=['POST'])
 def operator():
     return ControllerOperator().handleRequest()
 
-@app.route('/loginOwner', methods=['GET', 'POST'])
-def loginOwner():
+@app.route('/login/<role>', methods=['GET', 'POST'])
+def login(role):
+    if(role == 'owner'):
+        return ControllerOwner().handleLogin()
+    elif(role == 'operator'):
+        return ControllerOperator().handleLogin()
+    elif(role == 'customer'):
+        return ControllerCustomer().handleLogin()
+    else:
+        return '404'
     if(request.method == 'POST'):
         controllerObj = ControllerOwner()
         controllerObj.handleLogin()
@@ -63,15 +68,17 @@ def loginOwner():
             session.permanent = True
             return redirect('/')
         else:
-            return render_template('loginOwner.html', 
+            return render_template('login.html', 
                 username = label.username,
-                password = label.password
+                password = label.password,
+                toUrl = 'loginOwner'
             )
 
     else:
-        return render_template('loginOwner.html', 
+        return render_template('login.html', 
             username = label.username,
-            password = label.password
+            password = label.password,
+            toUrl = 'loginOwner'
         )
 
 
@@ -81,12 +88,59 @@ def logoutOwner():
     controllerObj.handleLogout()
     flash(controllerObj.response_data[label.details])
     if(controllerObj.response_data[label.success] == True):
-        return render_template('loginOwner.html',
+        return render_template('login.html',
             username = label.username,
-            password = label.password
+            password = label.password,
+            toUrl = 'loginOwner'
         )
     else:
         pass
+
+
+@app.route('/loginOperator', methods=['GET', 'POST'])
+def loginOperator():
+    if(request.method == 'POST'):
+        controllerObj = ControllerOperator()
+        controllerObj.handleLogin()
+        flash(controllerObj.response_data[label.details])    
+
+        if(controllerObj.response_data[label.success] == True):
+            session.permanent = True
+            return redirect('/')
+        else:
+            return render_template('login.html', 
+                username = label.username,
+                password = label.password,
+                toUrl = 'loginOperator'
+            )
+
+    else:
+        return render_template('login.html', 
+            username = label.username,
+            password = label.password,
+            toUrl = 'loginOperator'
+        )
+
+
+@app.route('/logoutOperator', methods=['GET'])
+def logoutOperator():
+    controllerObj = ControllerOperator()
+    controllerObj.handleLogout()
+    flash(controllerObj.response_data[label.details])
+    if(controllerObj.response_data[label.success] == True):
+        return render_template('login.html',
+            username = label.username,
+            password = label.password,
+            toUrl = 'loginOperator'
+        )
+    else:
+        pass
+
+@app.route('/viewSchedules')
+def viewSchedules():
+    controllerObj = ControllerOperator()
+    controllerObj.handleViewSchedule()
+    return render_template('table.html', response_data= controllerObj.response_data)
 
 @app.route('/debug')
 def debug():
